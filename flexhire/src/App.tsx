36ee95fd49ce5aca400ce6180624b4759c9b377b
Sian Richards <sian_richards@hotmail.co.uk>
2022-11-18 00:00:05 -0500

    receive & display job data

--- flexhire/src/App.tsx
+++ flexhire/src/App.tsx
@@ -1,83 +1,124 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import fetchFlexhireData from './apiCalls';
-import { currentUserQuery } from './queryTypes';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import fetchFlexhireData from "./apiCalls";
+import { currentUserQuery, jobQuery } from "./queryTypes";
 
 interface ICurrentUser {
   data: {
-  currentUser?: {
-    name: string,
-    avatarUrl: string,
-    userSkills: [
-      {
-      experience: string,
-      skill: {
-        name: string
-      }
-    }
-    ],
-    answers: [
-      {
-      question: {
-        title: string,
-        videoAnswer: {
-          video: {
-            url: string
-          }
+    currentUser?: {
+      name: string;
+      avatarUrl: string;
+      userSkills: [
+        {
+          experience: string;
+          skill: {
+            name: string;
+          };
         }
-      }
-    }
-    ]
-  }}
+      ];
+      answers: [
+        {
+          question: {
+            title: string;
+            videoAnswer: {
+              video: {
+                url: string;
+              };
+            };
+          };
+        }
+      ];
+    };
+  };
+}
+
+interface IJobs {
+  data: {
+    contract: {
+      client: {
+        name: string;
+      };
+      contractRequests: {
+        project: {
+          title: string;
+        };
+      };
+      firm: {
+        name: string;
+      };
+      job: {
+        title: string;
+      };
+    };
+  };
 }
 
 function App() {
-
-  const [profileInformation, setProfileInformation] = useState<Partial<ICurrentUser>>({})
+  const [profileInformation, setProfileInformation] = useState<
+    Partial<ICurrentUser>
+  >({});
+  const [jobInformation, setJobInformation] = useState<Partial<IJobs>>({});
 
   useEffect(() => {
     fetchFlexhireData(currentUserQuery).then((currentUserInfo) => {
-      setProfileInformation(currentUserInfo)
-    })
-  }, [])
+      setProfileInformation(currentUserInfo);
+    });
+    fetchFlexhireData(jobQuery).then((jobInfo) => {
+      setJobInformation(jobInfo);
+    });
+  }, []);
 
   // useEffect(() => {
-  //   console.log(profileInformation, Object.keys(profileInformation).length)
-  // }, [profileInformation])
+  //   console.log(jobInformation);
+  // }, [jobInformation]);
 
-  return (
-    profileInformation.data?.currentUser ? 
+  return profileInformation.data?.currentUser ? (
     <div className="App">
-    <header className="App-header">
-      <img src={profileInformation.data.currentUser.avatarUrl} className="App-logo" />
-      <p>
-        {profileInformation.data.currentUser.name}
-      </p>
-      <div>
-      <h2>Skills</h2>
-      {profileInformation.data.currentUser.userSkills.map((userSkill) => {
-        return (
-          <>
-          <div>{userSkill.skill.name}</div>
-        <div>Level: {userSkill.experience}/5</div>
-        </>
-        )
-      })}
-      </div>
-      <div>
-      {profileInformation.data.currentUser.answers.map((answer) => {
-        return (
-          <>
-          <div>{answer.question.title}</div>
-        <video controls><source src={answer.question.videoAnswer.video.url} type="video/mp4" /></video>
-        </>
-        )
-      })}
-      </div>
-    </header>
-  </div> :
-  <div>loading</div>
-  )
+      <header className="App-header">
+        <img
+          src={profileInformation.data.currentUser.avatarUrl}
+          className="App-logo"
+        />
+        <p>{profileInformation.data.currentUser.name}</p>
+        <div>
+          <h2>Skills</h2>
+          {profileInformation.data.currentUser.userSkills.map((userSkill) => {
+            return (
+              <>
+                <div>{userSkill.skill.name}</div>
+                <div>Level: {userSkill.experience}/5</div>
+              </>
+            );
+          })}
+        </div>
+        <div>
+          {profileInformation.data.currentUser.answers.map((answer) => {
+            return (
+              <>
+                <div>{answer.question.title}</div>
+                <video controls>
+                  <source
+                    src={answer.question.videoAnswer.video.url}
+                    type="video/mp4"
+                  />
+                </video>
+              </>
+            );
+          })}
+        </div>
+      </header>
+      {jobInformation.data && (
+        <div>
+          <h2>Jobs</h2>
+          <p>Title: {jobInformation.data.contract.job.title}</p>
+          <p>Company: {jobInformation.data.contract.firm.name}</p>
+          <p>Hiring Manager: {jobInformation.data.contract.client.name}</p>
+        </div>
+      )}
+    </div>
+  ) : (
+    <div>loading</div>
+  );
 }
 
 export default App;
