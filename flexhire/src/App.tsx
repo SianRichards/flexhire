6122b4686dd32020170c60d3ddf731e48cf6dc5c
Sian Richards <sian_richards@hotmail.co.uk>
2022-11-20 15:11:56 -0500

    api key form

--- flexhire/src/App.tsx
+++ flexhire/src/App.tsx
@@ -63,70 +63,96 @@ function App() {
     Partial<ICurrentUser>
   >({});
   const [jobInformation, setJobInformation] = useState<Partial<IJobs>>({});
+  const [formValue, setFormValue] = useState("");
+  const [userDoesNotExistMessage, setUserDoesNotExistMessage] = useState("")
+
+  const getProfileData = async (apiKey: string) => {
+    const currentUserInfo = await fetchFlexhireData(currentUserQuery, apiKey);
+    setProfileInformation(currentUserInfo);
+  };
+  const getJobData = async (apiKey: string) => {
+    const jobInfo = await fetchFlexhireData(jobQuery, apiKey);
+    setJobInformation(jobInfo);
+  };
 
   useEffect(() => {
-    const getProfileData = async () => {
-      const currentUserInfo = await fetchFlexhireData(currentUserQuery);
-      setProfileInformation(currentUserInfo);
-    };
-    const getJobData = async () => {
-      const jobInfo = await fetchFlexhireData(jobQuery);
-      setJobInformation(jobInfo);
-    };
-    getProfileData();
-    getJobData();
-  }, []);
+    if (profileInformation.data?.currentUser === null) {
+      setUserDoesNotExistMessage("Unable to find a user associated with with API key, please try another key.")
+    }
+    else setUserDoesNotExistMessage("")
+  }, [profileInformation]);
 
-  return profileInformation.data?.currentUser ? (
+  const handleSubmit = (event: any) => {
+    event.preventDefault()
+    getProfileData(formValue);
+    getJobData(formValue);
+  };
+
+  return (
     <div className="App">
       <header className="App-header">
-        <img
-          src={profileInformation.data.currentUser.avatarUrl}
-          className="App-logo"
-        />
-        <p>{profileInformation.data.currentUser.name}</p>
-        <div>
-          <h2>Skills</h2>
-          {profileInformation.data.currentUser.userSkills.map((userSkill) => {
-            return (
-              <>
-                <div>{userSkill.skill.name}</div>
-                <div>Level: {userSkill.experience}/5</div>
-              </>
-            );
-          })}
-        </div>
-        <div>
-          {profileInformation.data.currentUser.answers.map((answer) => {
-            return (
-              <>
-                <div>{answer.question.title}</div>
-                <video controls>
-                  <source
-                    src={answer.question.videoAnswer.video.url}
-                    type="video/mp4"
-                  />
-                </video>
-              </>
-            );
-          })}
-        </div>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Enter Flexhire API Key here:
+            <input
+              type="text"
+              value={formValue}
+              onChange={(event) => setFormValue(event?.target.value)}
+            />
+          </label>
+          <input type="submit" />
+        </form>
+        <p>{userDoesNotExistMessage}</p>
+        {profileInformation.data?.currentUser && (
+          <>
+            <img
+              src={profileInformation.data.currentUser.avatarUrl}
+              className="App-logo"
+            />
+            <p>{profileInformation.data.currentUser.name}</p>
+            <div>
+              <h2>Skills</h2>
+              {profileInformation.data.currentUser.userSkills.map(
+                (userSkill) => {
+                  return (
+                    <>
+                      <div>{userSkill.skill.name}</div>
+                      <div>Level: {userSkill.experience}/5</div>
+                    </>
+                  );
+                }
+              )}
+            </div>
+            <div>
+              {profileInformation.data.currentUser.answers.map((answer) => {
+                return (
+                  <>
+                    <div>{answer.question.title}</div>
+                    <video controls>
+                      <source
+                        src={answer.question.videoAnswer.video.url}
+                        type="video/mp4"
+                      />
+                    </video>
+                  </>
+                );
+              })}
+            </div>
+          </>
+        )}
       </header>
-      {jobInformation.data && (
+      {jobInformation.data &&
         jobInformation.data.contracts.nodes.map((job) => {
           return (
             <div>
-            <h2>Jobs</h2>
-            <p>Title: {job.job.title}</p>
-            <p>Company: {job.firm.name}</p>
-            <p>Hiring Manager: {job.client.name}</p>
-          </div>
-          )
-        })
-      )}
+              <h2>Jobs</h2>
+              <p>Title: {job.job.title}</p>
+              <p>Company: {job.firm.name}</p>
+              <p>Hiring Manager: {job.client.name}</p>
+            </div>
+          );
+        })}
     </div>
-  ) : (
-    <div>loading</div>
   );
 }
 
